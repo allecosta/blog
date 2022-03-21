f93c2f98195ac678c4620f9f59856df625d470a4
allecosta <alle_costa10@outlook.com>
2022-03-21 16:41:12 -0300

    Add head section

---
+++ includes/headsection.php
@@ -0,0 +1,6 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine">
+    <link rel="stylesheet" href="static/css/style.css">
+    <meta charset="UTF-8">
