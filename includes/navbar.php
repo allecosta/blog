c250feb23f6a2a9192f0d32723fc67519ea5c38a
allecosta <alle_costa10@outlook.com>
2022-03-21 16:41:41 -0300

    Add navbar

---
+++ includes/navbar.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+    <div class="logo-div">
+        <a href="index.php">
+            <h1>VidaBelaBlog</h1>
+        </a>
+    </div>
+    <ul>
+        <li><a class="active" href="index.php">Início</a></li>
+        <li><a href="#news">Notícias</a></li>
+        <li><a href="#contact">Contatos</a></li>
+        <li><a href="#about">Sobre</a></li>
+    </ul>
+</div>
