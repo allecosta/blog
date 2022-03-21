3323bf6b1361d7c483e558db5ea136e9570c75e6
allecosta <alle_costa10@outlook.com>
2022-03-21 16:39:45 -0300

    Add banner

---
+++ includes/banner.php
@@ -0,0 +1,20 @@
+<div class="banner">
+    <div class="welcome-msg">
+        <h1>Inspiração de hoje</h1>
+        <p>
+            Um dia sua vida<br>
+            piscará diante de seus olhos<br>
+            Verifique se vale a pena assistir<br>
+            Faça o seu melhor hoje!
+        </p>
+        <a href="register.php" class="btn">Junte-se a nós!</a>
+    </div>
+    <div class="login-div">
+        <form action="index.php" method="POST">
+            <h2>Login</h2>
+            <input type="text" name="username" placeholder="Nome de usuário">
+            <input type="password" name="password" placeholder="Senha">
+            <button class="btn" type="submit" name="login-btn">Entrar</button>
+        </form>
+    </div>
+</div>
