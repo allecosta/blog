90436eab27ff314c7bd66d59de77f8c40373580f
allecosta <alle_costa10@outlook.com>
2022-03-22 18:58:04 -0300

    Add conexao com o database

--- config.php
+++ config.php
@@ -2,5 +2,16 @@
 
 session_start();
 
+$server = 'localhost';
+$username = '###';
+$password = '###';
+$dbname = 'db_vidabelablog';
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Ops! Erro de conexão com o database." . mysqli_connect_error());
+}
+
 define('ROOT_PATH', realpath(dirname(__FILE__)));
 define('BASE_URL', 'http://localhost:8000/');
