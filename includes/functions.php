538440e8d4bbc6733575570289b307c37ae27fcf
allecosta <alle_costa10@outlook.com>
2022-03-24 16:59:25 -0300

    feat: Add novas funcoes

--- includes/functions.php
+++ includes/functions.php
@@ -9,5 +9,60 @@ function getPublishedPosts()
     $sql = "SELECT * FROM posts WHERE published=true";
     $result = mysqli_query($conn, $sql);
     $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    return $posts;
+
+    $finalPosts = array();
+    foreach ($posts as $post) {
+        $post['topics'] = getPostsTopics($post['id']);
+        array_push($finalPosts, $post);
+    }
+
+    return $finalPosts;
+}
+
+/**
+ * Recebe o ID da postagem, e retorna o topico da postagem
+ */
+function getPostsTopics($postsId)
+{
+    global $conn;
+    $sql = "SELECT * FROM topics WHERE id=
+        (SELECT topics_id FROM posts_topics WHERE posts_id=$postsId) LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    $topics = mysqli_fetch_assoc($result);
+    return $topics;
+}
+
+/**
+ * Retorna todas as postagens em um tópico
+ */
+function getPublishedPostsByTopics($topicsId)
+{
+    global $conn;
+    $sql = "SELECT * FROM posts ps WHERE ps.id IN 
+        (SELECT pt.posts_id FROM posts_topics pt 
+            WHERE pt.topics_id=$topicsId GROUP BY pt.posts_id
+                HAVING COUNT(1) = 1)";
+    $result = mysqli_query($conn, $sql);
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $finalPosts = array();
+
+    foreach ($posts as $post) {
+        $post['topics'] = getPostsTopics($post['id']);
+        array_push($finalPosts, $post);
+    }
+
+    return $finalPosts;
+}
+
+/**
+ * Retorna o nome do tópico por ID do tópico
+ */
+function getTopicsNameById($id)
+{
+    global $conn;
+    $sql = "SELECT name FROM topics WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    $topics = mysqli_fetch_assoc($result);
+    return $topics['name'];
 }
