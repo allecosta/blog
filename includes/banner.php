4f41d11344a002356b029e8b50a047e3dde898fa
allecosta <alle_costa10@outlook.com>
2022-03-28 17:05:56 -0300

    feat: exibicao do nome de usuario e botao-link de logout

--- includes/banner.php
+++ includes/banner.php
@@ -1,20 +1,31 @@
-<div class="banner">
-    <div class="welcome-msg">
-        <h1>Inspiração de hoje</h1>
-        <p>
-            Um dia sua vida<br>
-            piscará diante de seus olhos<br>
-            Verifique se vale a pena assistir<br>
-            Faça o seu melhor hoje!
-        </p>
-        <a href="register.php" class="btn">Junte-se a nós!</a>
+<?php if (isset($_SESSION['user']['username'])) { ?>
+    <div class="logged-in-info">
+        <span>Bem vindo <?php echo $_SESSION['user']['username'] ?></span>
+        |
+        <span><a href="logout.php">Sair</a></span>
     </div>
-    <div class="login-div">
-        <form action="index.php" method="POST">
-            <h2>Login</h2>
-            <input type="text" name="username" placeholder="Nome de usuário">
-            <input type="password" name="password" placeholder="Senha">
-            <button class="btn" type="submit" name="login-btn">Entrar</button>
-        </form>
+<?php } else { ?>
+    <div class="banner">
+        <div class="welcome-msg">
+            <h1>Inspiração de hoje</h1>
+            <p>
+                Um dia sua vida<br>
+                piscará diante de seus olhos<br>
+                Verifique se vale a pena assistir<br>
+                Faça o seu melhor hoje!
+            </p>
+            <a href="register.php" class="btn">Junte-se a nós!</a>
+        </div>
+        <div class="login-div">
+            <form action="<?php echo BASE_URL . 'index.php'; ?>" method="POST">
+                <h2>Login</h2>
+                <div style="width: 60%; margin: 0px auto;">
+                    <?php include(ROOT_PATH . '/includes/errors.php') ?>
+                </div>
+                <input type="text" name="username" placeholder="Nome de usuário">
+                <input type="password" name="password" placeholder="Senha">
+                <button class="btn" type="submit" name="login-btn">Entrar</button>
+            </form>
+        </div>
     </div>
-</div>
+<?php } ?>
