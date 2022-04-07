ec2fc7e5d51ef0ad7ab53eb60c2ddc18a5e9b421
allecosta <alle_costa10@outlook.com>
2022-04-07 17:25:23 -0300

    refactor: renomear arquivo de estilo

--- includes/headsection.php
+++ includes/headsection.php
@@ -2,5 +2,5 @@
 
 <head>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine">
-    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/public_styling.css">
     <meta charset="UTF-8">
