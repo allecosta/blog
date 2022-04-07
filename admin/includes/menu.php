74f5a4d0ffaee0d7e90aac1f771ff5cd2fa24bf9
allecosta <alle_costa10@outlook.com>
2022-04-07 17:31:19 -0300

    add menu admin

---
+++ admin/includes/menu.php
@@ -0,0 +1,13 @@
+<div class="menu">
+    <div class="card">
+        <div class="card-header">
+            <h2>Ação</h2>
+        </div>
+        <div class="card-content">
+            <a href="<?php echo BASE_URL . 'admin/create_post.php' ?>">Criar Posts</a>
+            <a href="<?php echo BASE_URL . 'admin/posts.php' ?>">Gerenciar Posts</a>
+            <a href="<?php echo BASE_URL . 'admin/users.php' ?>">Gerenciar Usuários</a>
+            <a href="<?php echo BASE_URL . 'admin/topics.php' ?>">Gerenciar Tópicos</a>
+        </div>
+    </div>
+</div>
